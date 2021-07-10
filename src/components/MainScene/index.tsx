9cd069ae1f1a66d4b0b39ca7098343c658b11303
Daniel Chen <cchen086@usc.edu>
2021-07-09 23:45:59 -0700

    feat: Add eb

--- src/components/MainScene/index.tsx
+++ src/components/MainScene/index.tsx
@@ -1,25 +1,31 @@
 import React, { useCallback, useRef, useState } from "react";
+import { useWindowSize } from "../../utils/hooks";
 
 import storeSrc from "../../images/store.jpeg";
-import { useWindowSize } from "../../utils/hooks";
+import ebSrc from "../../images/eb.png";
+import gaygaySrc from "../../images/gaygay.png";
+import guaigaiSrc from "../../images/guaigai.png";
+import xiabingSrc from "../../images/xiabing.png";
+import kaorouSrc from "../../images/kaorou.png";
+import quackboxSrc from "../../images/quackbox.png";
 
 import "./index.scss";
 
 export default function MainScene() {
   const viewport = useWindowSize();
   const [scrollX, setScrollX] = useState<Number>();
-  const imgRef = useRef<HTMLImageElement>(null);
+  const storeRef = useRef<HTMLImageElement>(null);
 
   const handleScroll = useCallback(
     (event: React.UIEvent<HTMLElement>) => {
-      if (viewport.height && viewport.width && imgRef.current?.clientWidth) {
+      if (viewport.height && viewport.width && storeRef.current?.clientWidth) {
         const scrollTop = event.currentTarget.scrollTop;
         const progress = scrollTop / (2000 - viewport.height);
-        const imgWidth = imgRef.current?.clientWidth;
+        const imgWidth = storeRef.current?.clientWidth;
         setScrollX(-(imgWidth - viewport.width) * progress);
       }
     },
-    [viewport]
+    [viewport, storeRef]
   );
   return (
     <div className="main-scene">
@@ -28,7 +34,12 @@ export default function MainScene() {
       </div>
       <div className="viewport">
         <div className="scene" style={{ transform: `translate(${scrollX}px)` }}>
-          <img src={storeSrc} alt="" ref={imgRef} />
+          <div className="store">
+            <img src={storeSrc} alt="" ref={storeRef} />
+          </div>
+          <div className="eb">
+            <img src={ebSrc} alt="" />
+          </div>
         </div>
       </div>
     </div>
