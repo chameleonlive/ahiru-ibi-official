46bc0f1af24050c8fd6a40f5b0d1d6240b4140fe
Daniel Chen <cchen086@usc.edu>
2021-07-11 23:24:06 -0700

    feat: Implement mouse hover nav

--- src/components/MainScene/index.tsx
+++ src/components/MainScene/index.tsx
@@ -1,6 +1,5 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useCallback, useEffect, useRef, useState } from "react";
 import { useWindowSize } from "../../utils/hooks";
-import useWindowScroll from "@react-hook/window-scroll";
 
 import storeSrc from "../../images/store.jpeg";
 import logoSrc from "../../images/logoShadow.png";
@@ -10,32 +9,49 @@ import guaigaiSrc from "../../images/guaigai.png";
 import xiabingSrc from "../../images/xiabing.png";
 import kaorouSrc from "../../images/kaorou.png";
 import quackboxSrc from "../../images/quackbox.png";
+import rightNavSrc from "../../images/right-nav.png";
+import leftNavSrc from "../../images/left-nav.png";
 
 import "./index.scss";
 
 export default function MainScene() {
   const viewport = useWindowSize();
-  const [scrollX, setScrollX] = useState<Number>();
-  const storeRef = useRef<HTMLImageElement>(null);
-  const scrollY = useWindowScroll(60);
+  const [scrollX, setScrollX] = useState<number>(0);
+  const [maxX, setMaxX] = useState<number>(0);
+  const [intervalId, setIntervalId] = useState<any>();
 
-  useEffect(() => {
-    if (viewport.height && viewport.width && storeRef.current?.clientWidth) {
-      const progress = scrollY / (2000 - viewport.height);
-      const imgWidth = storeRef.current?.clientWidth;
-      setScrollX(-(imgWidth - viewport.width) * progress);
-    }
-  }, [viewport, storeRef, scrollY]);
+  const onStoreLoad = useCallback(
+    (event) => {
+      if (viewport.width && event.currentTarget.clientWidth) {
+        const imgWidth = event.currentTarget.clientWidth;
+        const maxX = -(imgWidth - viewport.width);
+        setMaxX(maxX);
+      }
+    },
+    [viewport]
+  );
+
+  const moveRight = useCallback(() => {
+    setScrollX((scrollX: number | undefined) =>
+      Math.max((scrollX || 0) - 5, maxX)
+    );
+  }, [maxX]);
+
+  const moveLeft = useCallback(() => {
+    setScrollX((scrollX: number | undefined) =>
+      Math.min((scrollX || 0) + 5, 0)
+    );
+  }, []);
+
   return (
     <div className="main-scene">
-      <div className="dummy"></div>
       <div className="viewport">
         <div
           className="scene"
           style={{ transform: `translateX(${scrollX}px)` }}
         >
           <div className="store">
-            <img src={storeSrc} alt="" ref={storeRef} />
+            <img src={storeSrc} alt="" onLoad={onStoreLoad} />
           </div>
           <div className="item logo">
             <img src={logoSrc} alt="" />
@@ -59,6 +75,24 @@ export default function MainScene() {
             <img src={quackboxSrc} alt="" />
           </div>
         </div>
+      </div>
+      <div
+        className="left-edge"
+        onMouseOver={() => setIntervalId(setInterval(moveLeft, 10))}
+        onMouseOut={() => clearInterval(intervalId)}
+      >
+        <img src={leftNavSrc} alt="" style={{ opacity: scrollX < 0 ? 1 : 0 }} />
+      </div>
+      <div
+        className="right-edge"
+        onMouseOver={() => setIntervalId(setInterval(moveRight, 10))}
+        onMouseOut={() => clearInterval(intervalId)}
+      >
+        <img
+          src={rightNavSrc}
+          alt=""
+          style={{ opacity: scrollX > maxX ? 1 : 0 }}
+        />
       </div>
     </div>
   );
